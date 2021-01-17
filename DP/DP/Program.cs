a95eccecfd90192e6cbe156e7fcc432a24321a53
mihail.coscodan <mihail.coscodan@dassolutions.eu>
2021-01-17 09:40:24 +0200

    First commit

---
+++ DP/DP/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DP
+{        // C - Creational B- Behavioral, S-Structural
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Design Patterns");
+            CreationalPatterns();
+
+            Console.Read();
+        }
+
+        private static void CreationalPatterns()
+        {
+           
+        }
+    }
+}
